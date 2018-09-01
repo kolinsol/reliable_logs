09742b65543b0778a3817386e3c1af30b1522f3d
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2018-09-01 16:30:46 +0300

    add basic json processing functionality

--- apps/http_api/src/logs_handler.erl
+++ apps/http_api/src/logs_handler.erl
@@ -6,7 +6,28 @@ init(_Type, Req, []) ->
 	{ok, Req, no_state}.
 
 handle(Req, State) ->
+    has_body(Req),
     {ok, Req, State}.
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+has_body(Req) ->
+    case cowboy_req:has_body(Req) of
+        true -> is_json_body(Req);
+        false -> {error, no_body}
+    end.
+
+is_json_body(Req) ->
+    case cowboy_req:header(<<"content-type">>, Req) of
+        {<<"application/json">>, _Req2} ->
+            {ok, Body, _Req3} = cowboy_req:body(Req),
+            {Method, _Req4} = cowboy_req:method(Req),
+            validate_body(Body, Method);
+        _ -> {error, wrong_content_type}
+    end.
+
+validate_body(Body, <<"POST">>) ->
+    io:format("POST ~p~n", [Body]);
+validate_body(_Body, _Method) ->
+    {error, wrong_method}.
